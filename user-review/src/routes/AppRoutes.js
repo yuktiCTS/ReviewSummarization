bfd707f8cc711971e61c8be522bbcd539a6fd032
sai divya kanta <saidivya.kanta@rci.com>
2024-06-06 17:08:22 +0530

    changes

--- user-review/src/routes/AppRoutes.js
+++ user-review/src/routes/AppRoutes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
+import LoginPage from "../pages/LoginPage/LoginPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<Navigate to="/login" />}></Route> */}
+      <Route path="/" element={<Navigate to="/login" />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/home" element={<HomePage />}></Route>
       <Route path="/profile" element={<ProfilePage />}></Route>
     </Routes>
